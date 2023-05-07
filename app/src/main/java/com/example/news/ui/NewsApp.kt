650f2ef3f95169dc604fd348fa0c7d9c937fcacc
CherifiMi <cherifi154949@gmail.com>
2023-05-08 00:03:06 +0100

    .

--- app/src/main/java/com/example/news/ui/NewsApp.kt
+++ app/src/main/java/com/example/news/ui/NewsApp.kt
@@ -1,16 +1,25 @@
 package com.example.news.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.news.data.AppContainer
 import com.example.news.theme.NewsTheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsApp(
     appContainer: AppContainer,
@@ -28,7 +37,7 @@ fun NewsApp(
         val isExpandedScreen = widthSizeClass == WindowWidthSizeClass.Expanded
         val sizeAwareDrawerState = rememberSizeAwareDrawerState(isExpandedScreen)
 
-        ModelNaviagtionDrawer(
+        ModalNavigationDrawer(
             drawerContent = {
                 AppDrawer(
                     currentRoute = currentRoute,
@@ -40,3 +49,30 @@ fun NewsApp(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun rememberSizeAwareDrawerState(isExpandedScreen: Boolean): DrawerState {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+
+    return if (!isExpandedScreen) {
+        // If we want to allow showing the drawer, we use a real, remembered drawer
+        // state defined above
+        drawerState
+    } else {
+        // If we don't want to allow the drawer to be shown, we provide a drawer state
+        // that is locked closed. This is intentionally not remembered, because we
+        // don't want to keep track of any changes and always keep it closed
+        DrawerState(DrawerValue.Closed)
+    }
+}
+
+@Composable
+fun rememberContentPaddingForScreen(
+    additionalTop: Dp = 0.dp,
+    excludeTop: Boolean = false
+) =
+    WindowInsets.systemBars
+        .only(if (excludeTop) WindowInsetsSides.Bottom else WindowInsetsSides.Vertical)
+        .add(WindowInsets(top = additionalTop))
+        .asPaddingValues()
