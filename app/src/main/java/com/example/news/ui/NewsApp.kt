07e69237c2db6f96e93b36ae34652da70b44887b
CherifiMi <cherifi154949@gmail.com>
2023-05-07 15:36:34 +0100

    working

--- app/src/main/java/com/example/news/ui/NewsApp.kt
+++ app/src/main/java/com/example/news/ui/NewsApp.kt
@@ -1,8 +1,16 @@
 package com.example.news.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.example.news.data.AppContainer
+import com.example.news.theme.NewsTheme
 
 
 @Composable
@@ -10,5 +18,11 @@ fun NewsApp(
     appContainer: AppContainer,
     widthSizeClass: WindowWidthSizeClass
 ) {
-
+    NewsTheme() {
+        Surface() {
+            Column(Modifier.fillMaxSize().background(Color.Blue)) {
+                Text(text = "hillo")
+            }
+        }
+    }
 }
