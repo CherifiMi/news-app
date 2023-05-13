65b623b621120af3bfbc2b7d0413175b0d73ab0d
CherifiMi <cherifi154949@gmail.com>
2023-05-13 14:28:37 +0100

    .

--- app/src/main/java/com/example/news/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/news/ui/home/HomeViewModel.kt
@@ -1,6 +1,187 @@
 package com.example.news.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.news.R
+import com.example.news.data.posts.PostsRepository
+import com.example.news.model.Post
+import com.example.news.model.PostFeed
+import com.example.news.util.ErrorMessage
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import com.example.news.data.posts.Result
+import java.util.*
 
-class HomeViewModel() : ViewModel() {
+
+class HomeViewModel(
+    private val postsRepository: PostsRepository
+) : ViewModel() {
+    private val viewModelState = MutableStateFlow(HomeViewModelState(isLoading = true))
+    val uiState = viewModelState
+        .map(HomeViewModelState::toUiState)
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            viewModelState.value.toUiState()
+        )
+
+    init {
+        refreshPosts()
+        viewModelScope.launch {
+            postsRepository.observeFavorites().collect{fav ->
+                viewModelState.update { it.copy(favorites =  fav) }
+            }
+        }
+    }
+
+    fun refreshPosts(){
+        viewModelState.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            val result = postsRepository.getPostsFeed()
+            viewModelState.update {
+                when(result){
+                    is Result.Success -> {
+                        it.copy(postsFeed = result.data, isLoading = false)
+                    }
+                    is Result.Error -> {
+                        val errorMessages = it.errorMessages + ErrorMessage(
+                            id = UUID.randomUUID().mostSignificantBits,
+                            messageId = R.string.load_error
+                        )
+                        it.copy(errorMessages = errorMessages, isLoading = false)
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Toggle favorite of a post
+     */
+    fun toggleFavourite(postId: String) {
+        viewModelScope.launch {
+            postsRepository.toggleFavorite(postId)
+        }
+    }
+
+    /**
+     * Selects the given article to view more information about it.
+     */
+    fun selectArticle(postId: String) {
+        // Treat selecting a detail as simply interacting with it
+        interactedWithArticleDetails(postId)
+    }
+
+    /**
+     * Notify that an error was displayed on the screen
+     */
+    fun errorShown(errorId: Long) {
+        viewModelState.update { currentUiState ->
+            val errorMessages = currentUiState.errorMessages.filterNot { it.id == errorId }
+            currentUiState.copy(errorMessages = errorMessages)
+        }
+    }
+
+    /**
+     * Notify that the user interacted with the feed
+     */
+    fun interactedWithFeed() {
+        viewModelState.update {
+            it.copy(isArticleOpen = false)
+        }
+    }
+
+    /**
+     * Notify that the user interacted with the article details
+     */
+    fun interactedWithArticleDetails(postId: String) {
+        viewModelState.update {
+            it.copy(
+                selectedPostId = postId,
+                isArticleOpen = true
+            )
+        }
+    }
+
+    /**
+     * Notify that the user updated the search query
+     */
+    fun onSearchInputChanged(searchInput: String) {
+        viewModelState.update {
+            it.copy(searchInput = searchInput)
+        }
+    }
+
+    /**
+     * Factory for HomeViewModel that takes PostsRepository as a dependency
+     */
+    companion object {
+        fun provideFactory(
+            postsRepository: PostsRepository,
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return HomeViewModel(postsRepository) as T
+            }
+        }
+    }
+
+}
+
+
+
+
+
+sealed interface HomeUiState {
+
+    val isLoading: Boolean
+    val errorMessages: List<ErrorMessage>
+    val searchInput: String
+
+    data class NoPosts(
+        override val isLoading: Boolean,
+        override val errorMessages: List<ErrorMessage>,
+        override val searchInput: String
+    ) : HomeUiState
+    data class HasPosts(
+        val postsFeed: PostFeed,
+        val selectedPost: Post,
+        val isArticleOpen: Boolean,
+        val favorites: Set<String>,
+        override val isLoading: Boolean,
+        override val errorMessages: List<ErrorMessage>,
+        override val searchInput: String
+    ) : HomeUiState
+}
+
+private data class HomeViewModelState(
+    val postsFeed: PostFeed? = null,
+    val selectedPostId: String? = null,
+    val isArticleOpen: Boolean = false,
+    val favorites: Set<String> = emptySet(),
+    val isLoading: Boolean = false,
+    val errorMessages: List<ErrorMessage> = emptyList(),
+    val searchInput: String = "",
+) {
+    fun toUiState(): HomeUiState =
+        if (postsFeed == null) {
+            HomeUiState.NoPosts(
+                isLoading = isLoading,
+                errorMessages = errorMessages,
+                searchInput = searchInput
+            )
+        } else {
+            HomeUiState.HasPosts(
+                postsFeed = postsFeed,
+                selectedPost = postsFeed.allPosts.find {
+                    it.id == selectedPostId
+                } ?: postsFeed.highlightedPost,
+                isArticleOpen = isArticleOpen,
+                favorites = favorites,
+                isLoading = isLoading,
+                errorMessages = errorMessages,
+                searchInput = searchInput
+            )
+        }
 }
