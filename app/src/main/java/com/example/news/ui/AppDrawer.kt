0230ddba004215bcb001e91683b4298b9527c97e
CherifiMi <cherifi154949@gmail.com>
2023-05-13 13:17:45 +0100

    .

--- app/src/main/java/com/example/news/ui/AppDrawer.kt
+++ app/src/main/java/com/example/news/ui/AppDrawer.kt
@@ -1,11 +1,17 @@
 package com.example.news.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.news.R
+import com.example.news.theme.NewsTheme
 
 
 @Composable
@@ -17,8 +23,29 @@ fun AppDrawer(
     modifier: Modifier = Modifier
 ) {
 
-    Column(Modifier.background(Color.Red).fillMaxSize()) {
-        
+    Column(
+        Modifier
+            .background(Color.Red)
+            .fillMaxSize()
+    ) {
+
     }
-    
+
+}
+
+@Composable
+fun NewsLogo(modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_jetnews_logo),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(Modifier.width(8.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.ic_jetnews_logo),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
 }
